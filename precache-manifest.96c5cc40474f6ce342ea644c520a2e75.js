11e89e3bb10c34055c0c7b4f4f71373781fae888
taksh mehta <takshmehta111@gmail.com>
2020-07-22 20:22:17 +0530

    Updates

---
+++ precache-manifest.96c5cc40474f6ce342ea644c520a2e75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e169dc2d4ee2a053aae5bf41dd069b8",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "185b8647a56aac2c7e81",
+    "url": "/tic-tac-toe/static/css/2.23a9c941.chunk.css"
+  },
+  {
+    "revision": "14be09cc28d5e535a2f1",
+    "url": "/tic-tac-toe/static/css/main.4ba1bb0b.chunk.css"
+  },
+  {
+    "revision": "185b8647a56aac2c7e81",
+    "url": "/tic-tac-toe/static/js/2.e5599a1b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe/static/js/2.e5599a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14be09cc28d5e535a2f1",
+    "url": "/tic-tac-toe/static/js/main.a78453e9.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
